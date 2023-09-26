49fffa8540899f629e36afba14c95ac8b0cf50fe
Arsal <87937491+arsal214@users.noreply.github.com>
2023-09-26 19:44:00 +0500

    update

---
+++ resources/views/admin/payment/actions.blade.php
@@ -0,0 +1,30 @@
+@canany(['payments-view', 'payments-edit', 'payments-delete'])
+<div class="d-inline-flex">
+    <div class="dropdown">
+        <a href="#" class="text-body" data-bs-toggle="dropdown">
+            <i class="ph-list"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-end">
+            <form action="{{ route('payments.destroy',$payment->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                @can('payments-view')
+                <a href="{{ route('payments.show',$payment->id) }}" class="dropdown-item">
+                    <i class="ph-eye me-2"></i>{{ __('Show') }}
+                </a>
+                @endcan
+                @can('payments-edit')
+                <a href="{{ route('payments.edit',$payment->id) }}" class="dropdown-item">
+                    <i class="ph-note-pencil me-2"></i>{{ __('Edit') }}
+                </a>
+                @endcan
+                @can('payments-delete')
+                <button type="submit" class="dropdown-item sa-confirm">
+                    <i class="ph-trash me-2"></i>{{ __('Delete') }}
+                </button>
+                @endcan
+            </form>
+        </div>
+    </div>
+</div>
+@endcanany
--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -36,7 +36,7 @@
             <thead class="thead">
                 <tr>
                     <th>No</th>
-                    
+
 										<th>Interval Id</th>
 										<th>User Id</th>
 										<th>Date</th>
@@ -51,7 +51,7 @@
             @foreach ($payments as $key => $payment)
                 <tr>
                     <td>{{ ++$key }}</td>
-                    
+
 											<td>{{ $payment->interval_id }}</td>
 											<td>{{ $payment->user_id }}</td>
 											<td>{{ $payment->date }}</td>
@@ -67,36 +67,6 @@
     </div>
 </div>
 @endsection
-@canany(['payments-view', 'payments-edit', 'payments-delete'])
-<div class="d-inline-flex">
-    <div class="dropdown">
-        <a href="#" class="text-body" data-bs-toggle="dropdown">
-            <i class="ph-list"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-end">
-            <form action="{{ route('payments.destroy',$payment->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                @can('payments-view')
-                    <a href="{{ route('payments.show',$payment->id) }}" class="dropdown-item">
-                        <i class="ph-eye me-2"></i>{{ __('Show') }}
-                    </a>
-                @endcan
-                @can('payments-edit')
-                    <a href="{{ route('payments.edit',$payment->id) }}" class="dropdown-item">
-                        <i class="ph-note-pencil me-2"></i>{{ __('Edit') }}
-                    </a>
-                @endcan
-                @can('payments-delete')
-                    <button type="submit" class="dropdown-item sa-confirm">
-                        <i class="ph-trash me-2"></i>{{ __('Delete') }}
-                    </button>
-                @endcan
-            </form>
-        </div>
-    </div>
-</div>
-@endcanany
 @section('script')
 <script>
     $(function () {
