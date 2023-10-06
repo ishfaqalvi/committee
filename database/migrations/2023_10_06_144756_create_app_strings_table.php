f2996bc9cd911a32a13e07dddb97ce39f2bcfa32
Arsal <87937491+arsal214@users.noreply.github.com>
2023-10-06 14:55:24 +0500

    update

---
+++ database/migrations/2023_10_06_144756_create_app_strings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_strings', function (Blueprint $table) {
+            $table->id();
+            $table->string('language');
+            $table->string('key');
+            $table->string('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_strings');
+    }
+};
